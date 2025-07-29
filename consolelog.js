40bf3f675f78cbf907bd9f63a5bc3b28b4eb54c1
RENNYAWATANABE <127462554+RENNYAWATANABE@users.noreply.github.com>
2025-07-29 11:04:07 +0900

    Add files via upload

---
+++ consolelog.js
@@ -0,0 +1,28 @@
+
+console.log("consoleTEST確認");
+
+//変数宣言
+
+let number = 8888;
+console.log(number);
+
+//変数変更
+number = 10000;
+console.log(number);
+
+//if number >10000;
+//console.log("10000より大きい");
+//else console.log("100000より小さい");
+
+console.log("text表記");
+
+const name = 19990913
+console.log(name);
+
+
+
+
+
+
+
+
